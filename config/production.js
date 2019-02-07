eae92fc1483caa93b0759b2764a4ea1a55643fa5
idiot_shen <740624616@qq.com>
2019-02-07 22:03:08 +0800

    修改ignore

---
+++ config/production.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'secret': 'idiot_shen',
+  'db_uri': 'mongodb://111.231.105.43:27017/warehouse',
+  'user': 'idiot',
+  'pwd': 'shen1997.',
+  'redis_host': '127.0.0.1',
+  'redis_port': '6379',
+  'redis_expire_time': '1800'
+}
